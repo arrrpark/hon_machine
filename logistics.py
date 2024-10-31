1d0cef6514cfd4ef185018cabccd2add689a81fc
Park <arrrpark@icloud.com>
2024-10-31 12:52:59 -0700

    logistics added

--- logistics.py
+++ logistics.py
@@ -26,3 +26,32 @@ kn.fit(train_scaled, train_target)
 
 print(kn.score(train_scaled, train_target))
 print(kn.score(test_scaled, test_target))
+
+bream_smelt_indexes = (train_target == 'Bream') | (train_target == 'Smelt')
+train_bream_smelt = train_scaled[bream_smelt_indexes]
+target_bream_smelt = train_target[bream_smelt_indexes]
+
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression()
+lr.fit(train_bream_smelt, target_bream_smelt)
+print(lr.predict(train_bream_smelt[:5]))
+print(lr.predict_proba(train_bream_smelt[:5]))
+
+lr = LogisticRegression(C=20, max_iter=1000)
+lr.fit(train_scaled, train_target)
+print(lr.score(train_scaled, train_target))
+print(lr.score(test_scaled, test_target))
+print(lr.predict(test_scaled[:5]))
+
+proba = lr.predict_proba(test_scaled[:5])
+
+import numpy as np
+proba = lr.predict_proba(test_scaled[:5])
+print(np.round(proba, decimals=3))
+
+decision = lr.decision_function(test_scaled[:5])
+print(np.round(decision, decimals = 2))
+
+from scipy.special import softmax
+proba = softmax(decision, axis = 1)
+print(np.round(proba, decimals=3))
